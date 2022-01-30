0fe50f36e40b2d24695bad867900d89eb376822b
czarna7373 <czarna7373@gmail.com>
2022-01-30 14:50:30 +0100

    pierwsze

--- SPAwitrynaZal/src/app/app.module.ts
+++ SPAwitrynaZal/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+//Moje komponenty
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NawigacjaComponent } from './nawigacja/nawigacja.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NawigacjaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
