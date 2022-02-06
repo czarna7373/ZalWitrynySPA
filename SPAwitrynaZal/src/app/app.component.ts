3664edcecd7a6f90d18d876d31699b5c525631aa
czarna7373 <czarna7373@gmail.com>
2022-02-07 00:44:20 +0100

    zmiany

--- SPAwitrynaZal/src/app/app.component.ts
+++ SPAwitrynaZal/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SPAwitrynaZal';
+
+  update: boolean = false;
+
+  constructor(updates: SwUpdate, private data: DataService){
+    updates.available.subscribe(event => {
+
+      updates.activateUpdate().then(() => document.location.reload());
+    })
+  }
+
+  ngOnInit(){
+
+  }
 }
--- SPAwitrynaZal/src/app/app.module.ts
+++ SPAwitrynaZal/src/app/app.module.ts
@@ -6,16 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 //Moje komponenty
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NawigacjaComponent } from './nawigacja/nawigacja.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { BanerComponent } from './baner/baner.component';
 import { TrescComponent } from './tresc/tresc.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +27,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
@@ -39,7 +41,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule,
     NgbModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
